6c250ea856b04e37612608b4f009d028520179e3
Michał Kownacki <mk@michalkownacki.pl>
2022-03-24 14:15:00 +0100

    Refactor mkwc-image. Add DbSyncController.

---
+++ DbSyncController.js
@@ -0,0 +1,58 @@
+export class DbSyncController {
+  _host;
+  _path;
+  _noGet;
+  _getData;
+  _updateData;
+  _onDataReadyChange;
+  _onDataChange;
+  dataReady;
+  data;
+  constructor(host, getData, updateData, onDataReadyChange, onDataChange, options) {
+    this._host = host;
+    this._getData = getData;
+    this._updateData = updateData;
+    this._onDataReadyChange = onDataReadyChange;
+    this._onDataChange = onDataChange;
+    this._noGet = options.noGet;
+    host.addController(this);
+  }
+  _setDataReady(newDataReady) {
+    this.dataReady = newDataReady;
+    this._onDataReadyChange(this.dataReady);
+    if (newDataReady) {
+      this._host.dispatchEvent(new CustomEvent('data-ready', {detail: this.data}));
+    } else {
+      this._host.dispatchEvent(new CustomEvent('data-start-getting'));
+    }
+    return this.dataReady;
+  }
+  _setLocalData(newData) {
+    this.data = newData;
+    this._onDataChange(this.data);
+    return this.data;
+  }
+  async _syncLocalData() {
+    this._setDataReady(false);
+    const pathBeforeGettingData = this._path;
+    const gotData = await this._getData(this._path);
+    if (_.isEqual(this._path, pathBeforeGettingData)) {
+      this._setLocalData(gotData);
+      this._setDataReady(true);
+    }
+  }
+  setPath(newPath) {
+    if (!_.isEqual(this._path, newPath)) {
+      this._path = newPath;
+      if (!this._noGet) {
+        this._syncLocalData();
+      }
+    }
+  }
+  async requestDataUpdate(newData) {
+    const updatedData = await this._updateData(this._path, newData, this.data);
+    this._setLocalData(updatedData);
+    this._host.dispatchEvent(new CustomEvent('data-updated', {detail: this.data}));
+    return this.data;
+  }
+}
--- edit/mkwc-image.js
+++ edit/mkwc-image.js
@@ -1,24 +1,37 @@
-import {dbSyncMixin} from '../dbSyncMixin.js'
+import {DbSyncController} from '../DbSyncController.js';
 import {MkwcEditableImage} from './mkwc-editable-image.js';
 
-export class MkwcImage extends dbSyncMixin('image', MkwcEditableImage) {
-  static get properties() {
-    return {
-      image: Object,
-    };
-  }
-  constructor() {
-    super();
-    this.addEventListener('data-updated', async (event) => this.image = event.detail);
+export class MkwcImage extends MkwcEditableImage {
+  _dbSync;
+  static properties = {
+    path: Object, // Parameter passed to getData and updateData
+    noGet: Boolean,  // prevent from getting data when path is set
+    image: Object,
+  };
+  firstUpdated(changedProperties) {
+    this._dbSync = new DbSyncController(
+      this,
+      this.getData,
+      this.updateData,
+      (dataReady) => this.ready = dataReady,
+      (data) => this.image = data,
+      {noGet: this.noGet},
+    );
+    this.addEventListener('save', (event) => {
+      this._dbSync.requestDataUpdate(event.detail);
+    });
+    super.firstUpdated(changedProperties);
   }
   willUpdate(changedProperties) {
-    if (changedProperties.has('dataReady')) {
-      this.ready = this.dataReady;
-    }
     if ((changedProperties.has('image') || changedProperties.has('ready')) && this.ready) {
       this.src = this.image?.url;
     }
     super.willUpdate(changedProperties);
+  }
+  updated(changedProperties) {
+    if (changedProperties.has('path')) {
+      this._dbSync.setPath(this.path);
+    }
   }
 }
 customElements.define('mkwc-image', MkwcImage);
